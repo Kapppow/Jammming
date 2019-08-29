e9b297d9e66567fc54229b08b2c50768338cd7f5
Kapppow <49161923+Kapppow@users.noreply.github.com>
2019-08-29 08:55:28 -0400

    Set up project file structure

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,24 +1,24 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+
+// Components
+// import SearchBar from "./SearchBar/SearchBar"
+// import SearchResults from "./SearchResults/SearchResults"
+// import Playlist from "./Playlist/Playlist"
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>
+        Ja<span className="highlight">mmm</span>ing
+      </h1>
+      <div className="App">
+        {/* <!-- Add a SearchBar component --> */}
+        <div className="App-playlist">
+          {/* <!-- Add a SearchResults component --> */}
+          {/* <!-- Add a Playlist component --> */}
+        </div>
+      </div>
     </div>
   );
 }
